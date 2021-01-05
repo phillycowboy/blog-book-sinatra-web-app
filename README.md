5bc524bba76673ab3bda233d21c7b39b62d5552a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-01-04 20:51:39 -0500

    added create post

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 
 <!-- create models with relationships -->
 <!-- plan out routes for controller restful conventions -->
-<!-- build controller actions and views  -->
+<!-- build controller actions and passing appropriate data to views  -->
+
+<!-- build views forms -->
+<!-- build navigation -->
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,15 +3,28 @@ class BlogsController < ApplicationController
     # CREATE 
         # New
         # make a get request to '/blogs/new'
+        get '/blogs/new' do 
+           erb :'/blogs/new' 
+        end
 
         # Create
         # make a post request to '/blogs'
+        post '/blogs' do 
+            blog = Blog.new(params)
+            if !blog.title.empty? && !blog.author.empty? 
+                blog.save 
+                redirect '/blogs'
+            else
+                @error = "Oops! Please fill out all of the available forms."
+                erb :'/blogs/new' 
+            end
+        end
 
     # READ 
      # index - list all blogs
      # make a get request to '/blogs'
      get '/blogs' do 
-        @blogs = Blog.all 
+        @blogs = Blog.all.reverse
         erb :'blogs/index'
      end
     
@@ -27,6 +40,10 @@ class BlogsController < ApplicationController
 
         # Edit
             # making a get request to '/blogs/:id/edit'
+        get '/blogs/:id/edit' do 
+            @blog = Blog.find(params[:id])
+            erb :'/blogs/edit'
+        end
         
         # Update
             #make a patch request to '/blogs/:id' 
