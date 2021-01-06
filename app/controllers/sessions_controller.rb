a2cbbe77c6f6d0e624a8fb0f3073f0c1c2491069
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-01-06 17:29:34 -0500

    found a login bug, will fix tomorrow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,7 @@ post '/login' do
         @error = "Username and Password can't be blank" 
         erb :'users/login'      
     else 
-        if user = User.find_by(username: params["username"]) && user.authenticate(params["password"])
-              
+        if user = User.find_by(username: params["username"]) && user.authenticate(params["password"])  
               session[:user_id] = user.id
               redirect '/blogs'
         else
