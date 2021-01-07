38ddced59a6cab5e678e64cb22f490b0bf26aaab
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-01-07 12:19:48 -0500

    commit tree f0928d848c5ccfedb6557915484038e3e683824a

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210106184732) do
+ActiveRecord::Schema.define(version: 20210107171542) do
 
   create_table "blogs", force: :cascade do |t|
     t.string  "title"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20210106184732) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
